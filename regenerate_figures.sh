8eec88180f5c86883ff0721301102d96694ec339
Tifa <tiphereth-a@qq.com>
2022-06-19 15:02:46 +0800

    ci: add draw.io

--- regenerate_figures.sh
+++ regenerate_figures.sh
@@ -9,3 +9,8 @@ for i in imgcode/circo/*/*; do
   j=${i/'imgcode/circo'/'source/_posts'}
   cat $i | circo -Tsvg > ${j/.dot/.svg}
 done
+
+for i in imgcode/drawio/*; do
+  j=${i/'imgcode/drawio'/'source/_posts'}
+  draw.io -x -f svg -o ${i/'imgcode/drawio'/'source/_posts'} $i --embed-svg-images
+done
