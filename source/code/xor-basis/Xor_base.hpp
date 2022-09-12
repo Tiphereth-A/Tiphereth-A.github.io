7bd3e7b84cf34412a3a0b918604d9ae0507137bc
Tifa <62847935+Tiphereth-A@users.noreply.github.com>
2022-09-12 13:31:38 +0800

    refactor: xor_basis

--- source/code/xor-basis/Xor_base.hpp
+++ source/code/xor-basis/Xor_base.hpp
@@ -1,85 +1,71 @@
-template <std::size_t N = 64, std::enable_if_t<0 < N && N <= 64> * = nullptr>
-class Xor_base {
-  public:
-    using self = Xor_base<N>;
-    using data_type = std::uint64_t;
-    using size_type = std::size_t;
+template <std::size_t N = 64>
+class Xor_basis {
+    using self = Xor_basis<N>;
+    using data_t = std::bitset<N>;
     using reference = self &;
-    using iterator = data_type *;
-    using const_iterator = data_type *;
+    using iterator = data_t *;
+    using const_iterator = data_t *;
     using reverse_iterator = std::reverse_iterator<iterator>;
     using const_reverse_iterator = std::reverse_iterator<const_iterator>;
 
-    static const bool success = true;
-    static const bool failed = false;
-
-  private:
-    bool status;
-
   protected:
-    data_type base[N];
+    data_t base[N];
 
   public:
-    constexpr Xor_base() noexcept { this->clear(); }
-
-    explicit Xor_base(std::initializer_list<data_type> &&_list) noexcept:
-        Xor_base() {
-        for (auto &&i : std::move(_list)) this->insert(i);
+    constexpr Xor_basis() { this->clear(); }
+    constexpr Xor_basis(std::initializer_list<data_t> _list): Xor_basis() {
+        for (auto &&i : _list) this->insert(i);
     }
 
-    inline constexpr void clear() noexcept { memset(this->base, 0, sizeof(this->base)); }
+    constexpr void clear() {
+        for (size_t i = 0; i < size(); ++i) base[i].reset();
+    }
 
-    inline constexpr size_type get_len() const noexcept { return N; }
+    constexpr size_t size() const { return N; }
 
-    inline constexpr data_type &data(size_type index) noexcept { return this->base[index]; }
-    inline constexpr data_type &data(size_type index) const noexcept { return const_cast<self * const>(this)->base[index]; }
+    constexpr data_t operator[](size_t index) { return this->base[index]; }
+    constexpr data_t operator[](size_t index) const { return const_cast<self * const>(this)->base[index]; }
 
-    inline constexpr data_type operator[](size_type index) noexcept { return this->data(index); }
-    inline constexpr data_type operator[](size_type index) const noexcept { return const_cast<self * const>(this)->data(index); }
+    constexpr iterator begin() { return this->base; }
+    constexpr const_iterator begin() const { return const_cast<data_t * const>(this->base); }
 
-    inline constexpr iterator begin() noexcept { return this->base; }
-    inline constexpr const_iterator begin() const noexcept { return const_cast<data_type * const>(this->base); }
+    constexpr iterator end() { return this->begin() + this->size(); }
+    constexpr const_iterator end() const { return this->begin() + this->size(); }
 
-    inline constexpr iterator end() noexcept { return this->begin() + this->get_len(); }
-    inline constexpr const_iterator end() const noexcept { return this->begin() + this->get_len(); }
+    inline reverse_iterator rbegin() { return reverse_iterator(this->end()); }
+    inline const_reverse_iterator rbegin() const { return const_reverse_iterator(this->end()); }
 
-    inline reverse_iterator rbegin() noexcept { return reverse_iterator(this->end()); }
-    inline const_reverse_iterator rbegin() const noexcept { return const_reverse_iterator(this->end()); }
+    inline reverse_iterator rend() { return reverse_iterator(this->begin()); }
+    inline const_reverse_iterator rend() const { return const_reverse_iterator(this->begin()); }
 
-    inline reverse_iterator rend() noexcept { return reverse_iterator(this->begin()); }
-    inline const_reverse_iterator rend() const noexcept { return const_reverse_iterator(this->begin()); }
-
-    inline constexpr self &insert(data_type x) noexcept {
-        this->status = false;
-        for (size_type i = this->get_len() - 1; ~i; --i) {
-            if (!(x & (1ull << i))) continue;
-            if (this->data(i))
-                x ^= this->data(i);
+    constexpr bool insert(data_t x) {
+        bool status = false;
+        for (size_t i = size() - 1; ~i; --i) {
+            if (!(x[i])) continue;
+            if (base[i][i])
+                x ^= base[i];
             else {
-                for (size_type j = 0; j < i; ++j)
-                    if (x & (1ull << j)) x ^= this->data(j);
-                for (size_type j = i + 1; j < this->get_len(); ++j)
-                    if (this->data(j) & (1ull << i)) this->data(j) ^= x;
-                this->data(i) = x;
-                this->status = true;
+                for (size_t j = 0; j < i; ++j)
+                    if (x[j]) x ^= base[j];
+                for (size_t j = i + 1; j < size(); ++j)
+                    if (base[j][i]) base[j] ^= x;
+                base[i] = x;
+                status = true;
                 break;
             }
         }
-        return *this;
+        return status;
     }
 
-    inline constexpr bool get_status() const noexcept { return this->status; }
-
-    inline constexpr data_type max_span() const noexcept {
-        data_type ret(0);
-        for (const auto &i : this->base) ret ^= i;
+    constexpr data_t max_span() const {
+        data_t ret;
+        for (const auto &i : *this) ret ^= i;
         return ret;
     }
 
-    inline constexpr size_type rank() const noexcept {
-        size_type ret(0);
-        for (size_type i = 0; i < this->get_len(); ++i) ret += !!(this->base[i] & (1ull << i));
-        return ret;
+    constexpr size_t rank() const {
+        size_t res = 0;
+        for (size_t i = 0; i < size(); ++i) res += base[i][i];
+        return res;
     }
 };
-using xbase = Xor_base<>;
