77d950048f905cf6104f11d74f84699c1fb8e09c
Tifa <62847935+Tiphereth-A@users.noreply.github.com>
2022-11-19 00:38:16 +0800

    feat: upload

---
+++ source/code/ccpc-glr2022/A.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+inline void solve(int t_ = 0) {
+  string s;
+  cin >> s >> s;
+  for (auto &ch : s) ch = (ch == '.' ? 'C' : ch);
+  for (size_t i = 0; i < s.size(); ++i)
+    if (s[i] == 'L') {
+      if (i - 1 >= 0 && s[i - 1] == 'C') s[i - 1] = '.';
+      if (i + 1 < s.size() && s[i + 1] == 'C') s[i + 1] = '.';
+    }
+  cout << s << '\n';
+}
+int main() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  std::cerr << std::fixed << std::setprecision(6);
+  int i_ = 0;
+  solve(i_);
+  return 0;
+}
