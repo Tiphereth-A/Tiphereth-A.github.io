a9d310d75c4f8b0310a0ac5adcf460b93011c41c
Tifa <62847935+Tiphereth-A@users.noreply.github.com>
2022-11-26 20:21:19 +0800

    feat: upload

---
+++ source/code/ccpc-myr2022/A.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using ll = long long;
+#define for_(i, l, r, v...) for (ll i = (l), i##e = (r), ##v; i <= i##e; ++i)
+#define Rep for_
+template <class Tp>
+Tp dec(Tp &i) {
+  return --i;
+}
+template <class Tp>
+Tp inc(Tp &i) {
+  return ++i;
+}
+using namespace std;
+#define int long long
+inline void solve(int t_ = 0) {
+  const int INF = 1e14;
+  int n, k;
+  cin >> n >> k;
+  vector<int> A(2 * n + 1), B(2 * n + 1);
+  vector<vector<vector<int>>> Dp(
+    2 * n + 2, vector<vector<int>>(k + 2, vector<int>(k + 2, -INF)));
+  Rep(i, 1, n) cin >> A[i];
+  Rep(i, 1, n) cin >> B[i];
+  sort(A.begin() + 1, A.end(), greater<int>());
+  sort(B.begin() + 1, B.end(), greater<int>());
+  ll Ans = -1e14;
+  function<int(int, int, int)> DP = [&](int m, int a, int b) -> int {
+    if (m == 2 * n + 1) return 0;
+    if (Dp[m][a][b] != -INF) return Dp[m][a][b];
+    int Ret;
+    if (m & 1) {
+      Ret = -INF;
+      if (a != k && a != n) Ret = DP(m + 1, a + 1, b) + A[(m / 2) - b + a + 1];
+      Ret = max(Ret, DP(m + 1, a, b));
+    } else {
+      Ret = INF;
+      if (b != k && b != n) Ret = DP(m + 1, a, b + 1) - B[(m / 2) - a + b + 1];
+      Ret = min(Ret, DP(m + 1, a, b));
+    }
+    return Dp[m][a][b] = Ret;
+  };
+  cout << DP(1, 0, 0);
+}
+signed main() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  std::cerr << std::fixed << std::setprecision(6);
+  int i_ = 0;
+  solve(i_);
+  return 0;
+}
---
+++ source/code/ccpc-myr2022/M.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using ll = long long;
+#define for_(i, l, r, v...) for (ll i = (l), i##e = (r), ##v; i <= i##e; ++i)
+#define Rep for_
+template <class Tp>
+Tp dec(Tp &i) {
+  return --i;
+}
+template <class Tp>
+Tp inc(Tp &i) {
+  return ++i;
+}
+using namespace std;
+map<pair<char, char>, int> Map;
+inline void solve(int t_ = 0) {
+  string S;
+  cin >> S;
+  vector<int> A(S.size());
+  int Max = 0, Ans = 0;
+  Rep(i, 1, S.size() - 1) {
+    A[i] = A[i - 1] + Map[{S[i], S[i - 1]}];
+    if (A[i] > Max) Max = A[i], Ans = i;
+  }
+  cout << S[Ans] << '\n';
+}
+int main() {
+  Map[{'S', 'P'}] = 1;
+  Map[{'S', 'R'}] = -1;
+  Map[{'S', 'S'}] = 0;
+  Map[{'P', 'S'}] = -1;
+  Map[{'P', 'R'}] = 1;
+  Map[{'P', 'P'}] = 0;
+  Map[{'R', 'S'}] = 1;
+  Map[{'R', 'R'}] = 0;
+  Map[{'R', 'P'}] = -1;
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  std::cerr << std::fixed << std::setprecision(6);
+  int i_ = 0;
+  int t_ = 0;
+  std::cin >> t_;
+  for (i_ = 0; i_ < t_; ++i_) solve(i_);
+  return 0;
+}
