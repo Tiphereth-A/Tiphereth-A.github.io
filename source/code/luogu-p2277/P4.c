93ebd46dc390555601b9b3075472a7ed0b9cca41
Tifa <62847935+Tiphereth-A@users.noreply.github.com>
2023-01-10 18:19:39 +0800

    style: lint

--- source/code/luogu-p2277/P4.c
+++ source/code/luogu-p2277/P4.c
@@ -1,10 +1,14 @@
+// clang-format off
 #define _P2_HELPER(a, b, ...) a, b, __VA_ARGS__, b, a, __VA_ARGS__
 
-#define _P3_HELPER(a, b, c, ...)                                      \
-  _P2_HELPER(a, b, c, __VA_ARGS__), _P2_HELPER(b, c, a, __VA_ARGS__), \
-    _P2_HELPER(c, a, b, __VA_ARGS__)
+#define _P3_HELPER(a, b, c, ...)    \
+  _P2_HELPER(a, b, c, __VA_ARGS__), \
+  _P2_HELPER(b, c, a, __VA_ARGS__), \
+  _P2_HELPER(c, a, b, __VA_ARGS__)
 
 // use this
-#define P4                                                                \
-  _P3_HELPER(a, b, c, d), _P3_HELPER(b, c, d, a), _P3_HELPER(c, d, a, b), \
-    _P3_HELPER(d, a, b, c)
+#define P4                \
+  _P3_HELPER(a, b, c, d), \
+  _P3_HELPER(b, c, d, a), \
+  _P3_HELPER(c, d, a, b), \
+  _P3_HELPER(d, a, b, c)
